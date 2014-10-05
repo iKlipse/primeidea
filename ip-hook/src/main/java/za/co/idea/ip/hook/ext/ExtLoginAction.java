681fe6e769602c42b488512f95216c33d01bf83f
vmpattamatta-pss <vmpattamatta@pattamatta.co.in>
2014-10-05 14:49:31 +0200

    update

--- ip-hook/src/main/java/za/co/idea/ip/hook/ext/ExtLoginAction.java
+++ ip-hook/src/main/java/za/co/idea/ip/hook/ext/ExtLoginAction.java
@@ -1,60 +1,59 @@
 package za.co.idea.ip.hook.ext;
 
+import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletRequestWrapper;
-import javax.servlet.http.HttpServletResponse;
+import javax.portlet.PortletConfig;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
 
-import za.co.idea.ip.hook.NoSuchIpLoginException;
-import za.co.idea.ip.hook.NoSuchIpUserException;
 import za.co.idea.ip.hook.model.IpUser;
 import za.co.idea.ip.hook.service.IpLoginServiceUtil;
 import za.co.idea.ip.hook.service.IpUserServiceUtil;
 
+import com.liferay.portal.kernel.servlet.SessionErrors;
+import com.liferay.portal.kernel.struts.BaseStrutsPortletAction;
+import com.liferay.portal.kernel.struts.StrutsPortletAction;
+import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.Validator;
-import com.liferay.portal.model.CompanyConstants;
+import com.liferay.portal.model.Company;
+import com.liferay.portal.model.User;
 import com.liferay.portal.service.ServiceContext;
+import com.liferay.portal.service.ServiceContextFactory;
+import com.liferay.portal.theme.ThemeDisplay;
 import com.liferay.portal.util.PortalUtil;
 
-public class ExtLoginAction {
-	private static final String NAMESPACE_SERVLET_REQUEST_FQCN = "com.liferay.portal.servlet.NamespaceServletRequest";
-
-	public void processLogIn(long companyId, long userId, String handle, String password, boolean rememberMe, String authType, ActionResponse actionResponse, HttpServletRequest httpServletRequest, ServiceContext serviceContext) throws Exception {
-
-		if (httpServletRequest.getClass().getName().equals(NAMESPACE_SERVLET_REQUEST_FQCN)) {
-			while (httpServletRequest instanceof HttpServletRequestWrapper) {
-				HttpServletRequestWrapper httpServletRequestWrapper = (HttpServletRequestWrapper) httpServletRequest;
-				httpServletRequest = (HttpServletRequest) httpServletRequestWrapper.getRequest();
-			}
-		}
-
-		HttpServletResponse httpServletResponse = PortalUtil.getHttpServletResponse(actionResponse);
-
-		if (authType.equals(CompanyConstants.AUTH_TYPE_SN)) {
-			if (IpLoginServiceUtil.validateIpLogin(handle, password)) {
-				if (IpUserServiceUtil.validateLiferayUser(handle, companyId)) {
-					MethodInvoker.invokeLogin(httpServletRequest, httpServletResponse, handle, password, rememberMe, authType);
-					IpLoginServiceUtil.setLoginLastDate(handle);
+public class ExtLoginAction extends BaseStrutsPortletAction {
+	public void processAction(StrutsPortletAction originalStrutsPortletAction, PortletConfig portletConfig, ActionRequest actionRequest, ActionResponse actionResponse) throws Exception {
+		Company company = PortalUtil.getCompany(actionRequest);
+		String authType = company.getAuthType();
+		if (authType.equals("screenName")) {
+			String login = ParamUtil.getString(actionRequest, "login");
+			String password = actionRequest.getParameter("password");
+			if (IpLoginServiceUtil.validateIpLogin(login, password)) {
+				if (IpUserServiceUtil.validateLiferayUser(login, company.getCompanyId())) {
+					originalStrutsPortletAction.processAction(originalStrutsPortletAction, portletConfig, actionRequest, actionResponse);
 				} else {
-					IpUser ipUser = IpUserServiceUtil.getIpUser(handle, password);
+					ThemeDisplay themeDisplay = (ThemeDisplay) actionRequest.getAttribute("LIFERAY_SHARED_THEME_DISPLAY");
+					long userId = themeDisplay.getUserId();
+					IpUser ipUser = IpUserServiceUtil.getIpUser(login, password);
 					if (Validator.isNotNull(ipUser)) {
-
-						if (IpUserServiceUtil.addLiferayUser(ipUser.getUserId(), ipUser.getUserFName(), ipUser.getUserLName(), ipUser.getUserEmail(), password, ipUser.getUserScreenName(), userId, companyId, serviceContext)) {
-							MethodInvoker.invokeLogin(httpServletRequest, httpServletResponse, handle, password, rememberMe, authType);
-							IpLoginServiceUtil.setLoginLastDate(ipUser.getUserScreenName());
+						ServiceContext serviceContext = ServiceContextFactory.getInstance(User.class.getName(), actionRequest);
+						if (IpUserServiceUtil.addLiferayUser(ipUser.getUserId(), ipUser.getUserFName(), ipUser.getUserLName(), ipUser.getUserEmail(), password, ipUser.getUserScreenName(), userId, company.getCompanyId(), serviceContext)) {
+							originalStrutsPortletAction.processAction(originalStrutsPortletAction, portletConfig, actionRequest, actionResponse);
 						} else {
-							throw new NoSuchIpUserException();
+							SessionErrors.add(actionRequest, "pattamatta-no-such-user-exception");
 						}
-
 					}
 				}
 			} else {
-				throw new NoSuchIpLoginException();
+				SessionErrors.add(actionRequest, "pattamatta-no-such-user-exception");
 			}
 		} else {
-			MethodInvoker.invokeLogin(httpServletRequest, httpServletResponse, handle, password, rememberMe, authType);
+			originalStrutsPortletAction.processAction(originalStrutsPortletAction, portletConfig, actionRequest, actionResponse);
 		}
-
 	}
 
+	public String render(StrutsPortletAction originalStrutsPortletAction, PortletConfig portletConfig, RenderRequest renderRequest, RenderResponse renderResponse) throws Exception {
+		return originalStrutsPortletAction.render(originalStrutsPortletAction, portletConfig, renderRequest, renderResponse);
+	}
 }
